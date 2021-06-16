4a2f4cda3d093c413e224012df81da5f8267b632
Matheus Ribeiro de Sousa <matheusribs2012@gmail.com>
2021-06-16 11:34:44 -0300

    commit2.4

---
+++ versao-mobile/SQL.sql
@@ -0,0 +1,13 @@
+create database bdpesquisa;
+
+use bdpesquisa;
+
+create table perguntas (
+	
+    p1 int (11) not null,
+    p2 int (11) not null,
+    p3 int (11)  not null, 
+    p4 int (11) not null, 
+    p5 int (11) not null
+
+);
